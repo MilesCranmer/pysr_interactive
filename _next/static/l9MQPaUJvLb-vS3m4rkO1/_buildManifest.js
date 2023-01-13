c8395f0a2bcc26302e88e7d5145d05c60fdeebe8
MilesCranmer <miles.cranmer@gmail.com>
2023-01-13 03:52:04 +0000

    Deploying to gh-pages from @ MilesCranmer/pysr_interactive@4f62f42471b85725f26b9db473592605ec2bcc8f 🚀

--- _next/static/l9MQPaUJvLb-vS3m4rkO1/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/665f4d0a2b8e4f15.css","static/chunks/pages/index-e153ea2e7ef40508.js"],"/_error":["static/chunks/pages/_error-dfcfa5bb62767c20.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
